89496873e879c79ecc15e5eb3542e5de79621f20
Alan R Panosso <arpanosso@yahoo.com.br>
2023-09-05 15:16:28 -0300

    validação 2015

--- data/validacao-ano-2015.R
+++ data/validacao-ano-2015.R
@@ -7,6 +7,8 @@ library(ISLR)
 library(modeldata)
 library(vip)
 library(ggpubr)
+library(patchwork)
+source("R/my_functions.R")
 
 files_validacao <- list.files("data/Validacao/",full.names = TRUE)
 
@@ -47,7 +49,7 @@ df <- predict(fco2_modelo_load, new_data = data_set %>%
               filter(data == "2015-10-02"))
 
 cbind(df, data_set %>%
-        filter(data == "2015-11-18"))  %>%
+        filter(data == "2015-10-02"))  %>%
   ggplot(aes(x=.pred, y=F)) +
   geom_point()+
   theme_bw() +
@@ -62,4 +64,81 @@ cbind(df, data_set %>%
 cbind(df, data_set %>%
         filter(data == "2015-11-18")) %>%
   pull(F) %>% matrix(ncol=94) %>% image()
+
+## Objetivo é aplicar os p modelos |EU| para cada dia
+## em cada dia gerar um média dos modelos gerados
+## comparar com o dados de krigagem.
+
+
+file_models
+dias <- data_set %>%  pull(data) %>% unique()
+dis <- 100/93
+grid <- expand.grid(Y=seq(0,100,dis), X=seq(0,100,dis))
+dados_reais <- readxl::read_xlsx("data/dados-originais.xlsx")
+
+
+for(i in seq_along(dias)){
+  print(i)
+  df <- data_set %>%
+    filter(data == dias[i])
+  preds <- data.frame(Fobs = df$F)
+  for(j in seq_along(file_models)){
+    fco2_modelo_load <- read_rds(paste0("models/",file_models[j]))
+    pred <- predict(fco2_modelo_load, new_data = df) %>% as.vector()
+    preds <- cbind(preds,pred)
+  }
+  preds$media_models <- apply(preds[,2:6],1,mean)
+
+  plot_obs_vs_pred<-preds  %>% select(Fobs,media_models) %>%
+    ggplot(aes(x=media_models, y=Fobs)) +
+    geom_point()+
+    theme_bw() +
+    geom_smooth(method = "lm") +
+    stat_regline_equation(ggplot2::aes(
+      label =  paste(..eq.label.., ..rr.label.., sep = "*plain(\",\")~~")))
+  #print(plot_obs_vs_pred)
+
+  mp_krg <- tibble(grid, preds  %>% select(Fobs,media_models)) %>%
+    ggplot(aes(x=X,y=Y)) +
+    geom_tile(aes(fill = Fobs)) +
+    scale_fill_viridis_c() +
+    coord_equal()
+
+  mp_models <- tibble(grid, preds  %>% select(Fobs,media_models)) %>%
+    ggplot(aes(x=X,y=Y)) +
+    geom_tile(aes(fill = media_models)) +
+    scale_fill_viridis_c() +
+    coord_equal()
+
+  vetor_x <- dados_reais$X
+  vetor_y <- dados_reais$Y
+
+  print(mp_krg | mp_models)
+
+  my_data_frame <- tibble(grid, preds  %>% select(Fobs,media_models))
+
+  dia_teste <- paste0("F",
+    str_sub(dias[i],9,10),
+    str_sub(dias[i],6,7),
+    str_sub(dias[i],3,4)
+    )
+
+  reais <- dados_reais %>%
+    select(X,Y, starts_with(dia_teste))
+
+  names(reais) <- c("X","Y","Fobs")
+  graf_final <- reais %>%
+  group_by(X,Y) %>%
+    mutate(previsto = find_near_point(X,Y, my_data_frame,"est"),
+           observado = find_near_point(X,Y, my_data_frame)) %>%
+    ungroup() %>%
+    ggplot(aes(x=previsto, y=Fobs )) +
+     geom_point()+
+     theme_bw() +
+     geom_smooth(method = "lm") +
+     stat_regline_equation(ggplot2::aes(
+       label =  paste(..eq.label.., ..rr.label.., sep = "*plain(\",\")~~")))
+  print(graf_final)
+}
+
 
