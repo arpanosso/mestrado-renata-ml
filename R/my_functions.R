afd8e17f9bb480fbe5fd4482f1dadd741226a1b3
Alan R Panosso <arpanosso@yahoo.com.br>
2024-02-01 16:00:29 -0300

    modelo de médias

--- R/my_functions.R
+++ R/my_functions.R
@@ -1,6 +1,6 @@
 grd_read <- function(arq){
   nome <- str_split(arq, "/", simplify = TRUE)[3] %>%  str_remove(".grd")
-  dados <- read.table(arq, skip = 5) %>% as.tibble()
+  dados <- read.table(arq, skip = 5) %>% as_tibble()
   vetor <- as.vector(as.matrix(dados))
   id <- 1:length(vetor)
   data.frame(id,nome,vetor)
